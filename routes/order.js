81cb13a528ed5aee3402b6d591a32f95b41e18b3
Yasir45455 <149455805+Yasir45455@users.noreply.github.com>
2025-01-31 10:17:26 +0500

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authMiddleware = require('../middlewares/auth');
+const authMiddleware = require('../middleWares/auth');
 const orderController = require('../controllers/orderController');
 const router = express.Router();
 
